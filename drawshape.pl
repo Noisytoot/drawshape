30881fdef0cedb611917ad383077e427e7933aa1
Ron Nazarov <noisytoot@gmail.com>
2019-03-10 12:35:59 +0000

    Scalable Borders

--- drawshape.pl
+++ drawshape.pl
@@ -2,31 +2,44 @@
 use strict;
 use warnings;
 use POSIX;
-sub PrintBorderH {
-    my ($size, $fh) = @_;
-    my $b = ceil($size / 10);
-    print "$b\n";
-    print $fh "0";
-    for (my $i=1; $i <= $size - 2; $i++) {
-        print $fh " 1";
+use feature 'say';
+
+sub printBorderH {
+    my ($size, $fh, $b) = @_;
+    say "$b";
+    for (my $i=0; $i < $b; $i++) {
+	print $fh "0";
+	for (my $i=0; $i < $size - 2; $i++) {
+	    print $fh " 1";
+	}
+	print $fh " 0\n";
     }
-    print $fh " 0\n";
 }
 
 sub help {
-    print "DrawShape: Draws shapes (currently only squares) to a Portable BitMap (PBM) file\n\n";
-    print "Options:\n";
-    print "Help: no arguments (just drawshape)\n\n";
-    print "How to draw a shape:\n";
-    print "drawshape size filename\n";
-    print "Example:\n";
-    print "drawshape 60 shape0.pbm\n";
+    say "DrawShape: Draws shapes (currently only squares) to a Portable BitMap (PBM) file\n";
+    say "Help:";
+    say "Size must be more than 10\n";
+    say "How to draw a shape:";
+    say "drawshape size filename\n";
+    say "Example:";
+    say "drawshape 60 shape0.pbm";
+}
+
+sub borderV {
+    my $b = $_[0];
+    return "1 " x $b;
 }
 
 if ($#ARGV == -1) {
     help();
-} elsif ($#ARGV > 0) {# elsif = else if
+} elsif ($#ARGV > 0) {
     my ($size, $filename) = @ARGV;
+    if ($size < 10) {
+	say "Size must be more than 10";
+	exit 1;
+    }
+    my $b = ceil($size / 10);
     open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
     # Prints the PBM header
     print $fh "P1\n$size $size\n";
@@ -37,18 +50,18 @@ if ($#ARGV == -1) {
     }
     print $fh "0\n";
     
-    PrintBorderH($size, $fh);
+    printBorderH($size, $fh, $b);
     
-    # Prins the main square body
-    for (my $j=0; $j <= $size - 5; $j++) {
-        print $fh "0 1";
-        for (my $i=1; $i <= $size - 4; $i++) {
-            print $fh " 0";
+    # Prints the main square body
+    for (my $j=0; $j < $size - ($b * 2) - 2; $j++) {
+        print $fh "0 " . borderV($b);
+        for (my $i=0; $i < $size - ($b * 2) - 2; $i++) {
+            print $fh "0 ";
         }
-        print $fh " 1 0\n";
+        print $fh borderV($b) . "0" . "\n";
     }
     
-    PrintBorderH($size, $fh);
+    printBorderH($size, $fh, $b);
     
     # Prints the last line
     for (my $i=0; $i < $size - 1; $i++) {
